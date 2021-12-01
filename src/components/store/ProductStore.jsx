7867a30b34d13896a0567a683507bd156cbb0b98
bxxmi <bmi6638@gmail.com>
2021-12-01 14:01:17 +0900

    feat: implement product store

---
+++ src/components/store/ProductStore.jsx
@@ -0,0 +1,37 @@
+import { makeObservable, observable, action, computed } from 'mobx';
+
+class ProductStore {
+  // 구매한 상품들이 담길 배열
+  productList = [];
+
+  constructor() {
+    makeObservable(this, {
+      product: observable,
+      selectProduct: action,
+      totalPrice: computed
+    });
+  };
+
+  selectProduct = ({ productId, productName, productPrice }) => {
+   const selectedProduct = this.productList.find(product => productId === product.productId);
+
+   if (!selectedProduct) {
+     this.productList.push({
+      productId,
+      productName,
+      productPrice,
+      count: 1
+     });
+    } else {
+      selectedProduct.count++;
+    }
+  }
+
+  get totalPrice() {
+    return this.productList.reduce((prev, curr) => {
+      return prev + curr.productPrice * curr.count;
+    }, 0);
+  }
+}
+
+export default ProductStore;
